9919ffa3d6c5e084ccabb49a54fedf5ab2d5fd84
Arthur Littmann <arthur@lewagon.org>
2020-06-09 18:09:15 +0100

    modal content

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
+  has_many :items, dependent: :destroy
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -11,7 +12,7 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.email = auth.info.email
       user.github_avatar = auth.info.image
-      user.nickname = auth.nickname
+      user.nickname = auth.info.nickname
       user.password = Devise.friendly_token[0,20]
     end
   end
