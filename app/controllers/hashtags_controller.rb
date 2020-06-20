6699d9d5e62b2d96715db0c678bfdc90cce6d77c
Arthur Littmann <arthur@lewagon.org>
2020-06-20 09:32:44 +0100

    style

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -6,7 +6,7 @@ class HashtagsController < ApplicationController
     else
       @hashtags = policy_scope(Hashtag).includes(:items)
     end
-    @hashtags = @hashtags.sort_by { |h| h.items.count }
+    @hashtags = @hashtags.sort_by { |h| h.items.count }.first(5)
     respond_to do |format|
       format.json { render json: @hashtags.map { |h| { name: h.name, count: h.items.count } } }
     end
