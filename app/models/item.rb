9966331a026037de9bc730e78824171bf1a7b4b8
Arthur Littmann <arthur.littmann@hey.com>
2020-08-10 16:42:17 +0100

    a

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,8 +25,8 @@ class Item < ApplicationRecord
   scope :filter_by_city, -> (city_id) { where city_id: city_id }
 
   has_one_attached :photo
-  # validate :photo_present
-  # validate :photo_size, if: Proc.new { |p| p.photo.attached? }
+  validate :photo_present
+  validate :photo_size, if: Proc.new { |p| p.photo.attached? }
 
   after_create :notify_subscribers
 
